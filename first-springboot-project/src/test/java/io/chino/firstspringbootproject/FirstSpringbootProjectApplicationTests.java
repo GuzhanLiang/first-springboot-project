79cf4056d3edd570287d55d34cfcf50275578f42
chino <guzhanliang1@gmail.com>
2022-12-07 19:55:38 -0500

    first-springboot

---
+++ first-springboot-project/src/test/java/io/chino/firstspringbootproject/FirstSpringbootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.chino.firstspringbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringbootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
